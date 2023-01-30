2171d17cd58e2870a84b04aa6f42d2f51e1f6dbd
Erikgb11 <erikgb11@outlook.com>
2023-01-30 13:53:51 -0600

    sawtooth wave sequence b

---
+++ 5to semestre/teoria de comunicaciones y señales/p1_41b.m
@@ -0,0 +1,15 @@
+n = 0:50; 
+f = 0.05; 
+phase = 0;
+peak = 0.5;
+A = 2.0; 
+arg = 2*pi*f*n + phase; 
+x = A*sawtooth(arg,peak);
+clf; 
+stem(n,x); 
+axis([0 50 -2 2]);
+grid; 
+title('Sawtooth Wave Sequence of Fig. 1.2(b)');
+xlabel('Time index n');
+ylabel('Amplitude');
+axis;
