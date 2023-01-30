519c3618b496464ac33e3faf80e1e7a0d4f65c61
Erikgb11 <erikgb11@outlook.com>
2023-01-30 14:17:41 -0600

    registro entrada y salida serie

---
+++ 5to semestre/DSD/SISO/SISO.vhd
@@ -0,0 +1,223 @@
+
+--  CYPRESS NOVA XVL Structural Architecture
+
+--  JED2VHD Reverse Assembler - 6.3 IR 35
+
+
+--    VHDL File: SISO.vhd
+
+--    Date: Tue Dec 07 15:17:38 2021
+
+--  Disassembly from Jedec file for: c22v10
+
+--  Device Ordercode is: PALCE22V10-25PC/PI
+library ieee;
+use ieee.std_logic_1164.all;
+
+library primitive;
+use primitive.primitive.all;
+
+
+-- Beginning Test Bench Header
+
+ENTITY entradas2 IS
+    PORT (
+	                data :    in std_logic ;
+	               clock :    in std_logic ;
+	                  q0 : inout std_logic
+    );
+END entradas2;
+
+-- End of Test Bench Header
+
+ARCHITECTURE DSMB of entradas2 is
+
+	signal jed_node1	: std_logic:='0' ; -- clock
+	signal jed_node2	: std_logic:='0' ; -- data
+	signal jed_node3	: std_logic:='0' ;
+	signal jed_node4	: std_logic:='0' ;
+	signal jed_node5	: std_logic:='0' ;
+	signal jed_node6	: std_logic:='0' ;
+	signal jed_node7	: std_logic:='0' ;
+	signal jed_node8	: std_logic:='0' ;
+	signal jed_node9	: std_logic:='0' ;
+	signal jed_node10	: std_logic:='0' ;
+	signal jed_node11	: std_logic:='0' ;
+	signal jed_node12	: std_logic:='0' ;
+	signal jed_node13	: std_logic:='0' ;
+	signal jed_node15	: std_logic:='0' ;
+	signal jed_node16	: std_logic:='0' ;
+	signal jed_node17	: std_logic:='0' ;
+	signal jed_node18	: std_logic:='0' ;
+	signal jed_node19	: std_logic:='0' ;
+	signal jed_node20	: std_logic:='0' ;
+	signal jed_node21	: std_logic:='0' ;
+	signal jed_node22	: std_logic:='0' ;
+	signal jed_node23	: std_logic:='0' ;
+	signal jed_node24	: std_logic:='0' ;
+
+	for all: c22v10m use entity primitive.c22v10m (sim);
+
+SIGNAL  one:std_logic:='1';
+SIGNAL  zero:std_logic:='0';
+SIGNAL  jed_oept_1:std_logic:='0';
+--Attribute PIN_NUMBERS of clock:SIGNAL is "0001";
+
+SIGNAL  jed_oept_2:std_logic:='0';
+--Attribute PIN_NUMBERS of data:SIGNAL is "0002";
+
+SIGNAL  jed_oept_14:std_logic:='0';
+SIGNAL  jed_sum_14,jed_fb_14:std_logic:='0';
+--Attribute PIN_NUMBERS of q0:SIGNAL is "0014";
+
+SIGNAL  jed_oept_15:std_logic:='0';
+SIGNAL net747:std_logic:='0';
+SIGNAL  jed_sum_15,jed_fb_15:std_logic:='0';
+--Attribute PIN_NUMBERS of jed_node15:SIGNAL is "0015";
+
+SIGNAL  jed_oept_22:std_logic:='0';
+SIGNAL net732:std_logic:='0';
+SIGNAL  jed_sum_22,jed_fb_22:std_logic:='0';
+--Attribute PIN_NUMBERS of jed_node22:SIGNAL is "0022";
+
+SIGNAL  jed_oept_23:std_logic:='0';
+SIGNAL net758:std_logic:='0';
+SIGNAL  jed_sum_23,jed_fb_23:std_logic:='0';
+--Attribute PIN_NUMBERS of jed_node23:SIGNAL is "0023";
+
+SIGNAL  jed_oept_25:std_logic:='0';
+SIGNAL  jed_node25,jed_sum_25:std_logic:='0';
+SIGNAL  jed_oept_26:std_logic:='0';
+SIGNAL  jed_node26,jed_sum_26:std_logic:='0';
+
+BEGIN
+jed_node1 <= clock ;
+jed_node2 <= data ;
+Mcell_14:c22v10m
+generic map(reg,
+   ninv,
+   xreg,
+   	25 ns, --tpd
+	25 ns, --tea
+	25 ns, --ter
+	15 ns, --tco
+	18 ns, --ts
+	0 ns, --th
+	14 ns, --twh
+	14 ns, --twl
+	13 ns, --tcf
+	25 ns, --taw
+	25 ns, --tar
+	25 ns, --tap
+	25 ns  --tspr
+)
+port map(
+     d=>jed_sum_14,
+     clk=>jed_node1,
+     oe=>jed_oept_14,
+     ss=>jed_sum_26,
+     ar=>jed_sum_25,
+     y=>q0,
+     fb=>jed_fb_14
+   );
+
+Mcell_15:c22v10m
+generic map(reg,
+   ninv,
+   xreg,
+   	25 ns, --tpd
+	25 ns, --tea
+	25 ns, --ter
+	15 ns, --tco
+	18 ns, --ts
+	0 ns, --th
+	14 ns, --twh
+	14 ns, --twl
+	13 ns, --tcf
+	25 ns, --taw
+	25 ns, --tar
+	25 ns, --tap
+	25 ns  --tspr
+)
+port map(
+     d=>jed_sum_15,
+     clk=>jed_node1,
+     oe=>jed_oept_15,
+     ss=>jed_sum_26,
+     ar=>jed_sum_25,
+     y=>jed_node15,
+     fb=>jed_fb_15
+   );
+
+Mcell_22:c22v10m
+generic map(reg,
+   ninv,
+   xreg,
+   	25 ns, --tpd
+	25 ns, --tea
+	25 ns, --ter
+	15 ns, --tco
+	18 ns, --ts
+	0 ns, --th
+	14 ns, --twh
+	14 ns, --twl
+	13 ns, --tcf
+	25 ns, --taw
+	25 ns, --tar
+	25 ns, --tap
+	25 ns  --tspr
+)
+port map(
+     d=>jed_sum_22,
+     clk=>jed_node1,
+     oe=>jed_oept_22,
+     ss=>jed_sum_26,
+     ar=>jed_sum_25,
+     y=>jed_node22,
+     fb=>jed_fb_22
+   );
+
+Mcell_23:c22v10m
+generic map(reg,
+   ninv,
+   xreg,
+   	25 ns, --tpd
+	25 ns, --tea
+	25 ns, --ter
+	15 ns, --tco
+	18 ns, --ts
+	0 ns, --th
+	14 ns, --twh
+	14 ns, --twl
+	13 ns, --tcf
+	25 ns, --taw
+	25 ns, --tar
+	25 ns, --tap
+	25 ns  --tspr
+)
+port map(
+     d=>jed_sum_23,
+     clk=>jed_node1,
+     oe=>jed_oept_23,
+     ss=>jed_sum_26,
+     ar=>jed_sum_25,
+     y=>jed_node23,
+     fb=>jed_fb_23
+   );
+
+net747 <= jed_fb_15;
+net732 <= jed_fb_22;
+net758 <= jed_fb_23;
+jed_node25<=jed_sum_25;
+jed_node26<=jed_sum_26;
+jed_oept_14<=(one);
+
+jed_sum_14<= (((not(jed_fb_23))));
+
+jed_sum_15<= (((not(jed_fb_22))));
+
+jed_sum_22<= (((jed_node2)));
+
+jed_sum_23<= (((not(jed_fb_15))));
+
+end DSMB;
